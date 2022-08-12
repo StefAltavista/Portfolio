0a9065fb641f408588124eb2f3abb9ab1cb599cc
Stef Altavista <stefanomsc@hotmail.it>
2022-08-12 17:36:44 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const HTMLwebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./public/src/start.js",
     entry: [
+        "./public/src/start.js",
         path.join(__dirname, "public", "css", "style.css"),
         path.join(__dirname, "public", "src", "start.js"),
     ],
